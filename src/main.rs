7eb0bd834aebdabff8c3e83ae5f2102d65cd281f
wantJoy1 <wantjoy1@gmail.com>
2020-11-07 21:26:16 +0900

    read file and print it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
-/*
-fn main() {
-    println!("Hello, world!");
-}
-*/
+use std::env;
+use std::error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
+fn main() -> Result<(), Box<dyn error::Error>> {
+    eprintln!("START");
+    let args: Vec<_> = env::args().collect();
+    let ref fname_in = args[1];
+
+    for result in BufReader::new(File::open(fname_in)?).lines() {
+        let ll = result?;
+        println!("{}", ll);
+    }
+
+    eprintln!("FINISH");
+    Ok(())
+}
+
+/*
 extern crate yaml_rust;
 use yaml_rust::{YamlLoader};
 
+use std::fs;
+use std::io;
+use std::env;
+
 // https://github.com/chyh1990/yaml-rust の写経
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
+    // args[1]
+    let file = fs::read_to_string(args[1])?;
+    println!("{}", file);
+    Ok(())
     let s = 
         "
     servers:
@@ -22,3 +44,8 @@ fn main() {
 
    println!("{:?}", doc);
 }
+
+fn do_mkdir(dirname: String) {
+    fs::create_dir(dirname);
+}
+*/
