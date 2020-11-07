aebb47d221ea9dbf242d7cd08278a88f04767a17
wantJoy1 <wantjoy1@gmail.com>
2020-11-07 20:56:20 +0900

    コピペした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+/*
 fn main() {
     println!("Hello, world!");
+}
+*/
+
+extern crate yaml_rust;
+use yaml_rust::{YamlLoader};
+
+// https://github.com/chyh1990/yaml-rust の写経
+fn main() {
+    let s = 
+        "
+    servers:
+        - a.example.com
+        - b.example.com
+        - c.example.com
+        - d.example.com
+    ";
+    let docs = YamlLoader::load_from_str(s).unwrap();
+   let doc = &docs[0]; 
+
+   println!("{:?}", doc);
 }
